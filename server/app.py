807b15783ae2abaecd23f93ef7a8b12884ea6684
dmmuchoki7 <dmmuchoki7@gmail.com>
2024-03-26 15:09:58 +0300

    create and get all campaign route

--- server/app.py
+++ server/app.py
@@ -3,9 +3,17 @@ from flask import Flask
 from flask_migrate import Migrate
 from flask_restful import Api,Resource
 from models import db, User, Donation, Campaign, Organisation
+import os
+from dotenv import load_dotenv
+load_dotenv()
+from intasend import APIService
 from flask import request,jsonify,make_response
 from flask_bcrypt import Bcrypt
 
+#fetch environment variables  for the api key and server url
+token=os.getenv("INTA_SEND_API_KEY")
+publishable_key= os.getenv('PUBLISHABLE_KEY')
+service = APIService(token=token,publishable_key=publishable_key, test=True)
 
 app = Flask(__name__)
 
@@ -104,7 +112,60 @@ class userDataByid(Resource):
     
 api.add_resource(userDataByid, '/users/<int:id>')
 
+class campaignData(Resource):
+    # @jwt_required()
+    def get(self):
+        all_campaigns = [campaign.to_dict() for campaign in Campaign.query.all()]
+        response = make_response(jsonify(all_campaigns), 200)
+        return response
+    
+    # @jwt_required()
+    def post(self):
+        data=request.get_json()
+        name = data.get("name")
+        description = data.get("description")
+        startDate = data.get("startDate")
+        endDate = data.get("endDate")
+        targetAmount = float(data.get('targetAmount'))
+        isActive= data.get('isActive', True)
+        org_id= int(data.get('orgId'))
 
+        available_org= Organisation.query.get(org_id)
+        if not (name and description and startDate and endDate):
+            return jsonify({"Error":"Please provide complete information"}),400
+        elif not available_org:
+            return jsonify({"Error":"Organisation doesnot exist."}),404
+
+        newCampaign = Campaign(campaignName= name, 
+                               description= description, 
+                               startDate=startDate, 
+                               endDate= endDate, 
+                               targetAmount=targetAmount, 
+                               isActive= isActive,
+                               org_id= org_id
+                            )
+        #create wallet
+        response = service.wallets.create(currency="KES", label=f"{newCampaign.campaignName}", can_disburse=True)
+
+        if response.get('errors'):
+            return jsonify({"Error":response.get('errors')[0].get('detail')}),400
+
+        newCampaign.wallet_id=response.get("wallet_id")
+        try:
+            db.session.add(newCampaign)
+            db.session.commit()
+            return jsonify(newCampaign.to_dict(), 201)
+        except Exception as e:
+            print(e)
+            return jsonify({"Error": "Something went wrong while creating your campaign"}),500
+
+api.add_resource(campaignData, '/campaigns')
+
+class campaignItem(Resource):
+
+    # @jwt_required
+    def get(self, camp_id):
+        pass
 
 
 if __name__  =="__main__":
