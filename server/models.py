46ae4596f4594687a9a7ce3103b14b239c7db077
dmmuchoki7 <dmmuchoki7@gmail.com>
2024-06-11 01:14:21 +0300

    Update status function corrected and added states

--- server/models.py
+++ server/models.py
@@ -305,8 +305,8 @@ class  Campaign(db.Model, SerializerMixin):
             'orgDescription': self.organisation.orgDescription,
             'isVerified': self.organisation.isVerified,
             },
-            # 'donations': [donation.serialize() for donation in self.donations]
-            'donations': [donation.serialize() for donation in self.donations if donation.status == 'COMPLETE']
+            'donations': [donation.serialize() for donation in self.donations]
+            # 'donations': [donation.serialize() for donation in self.donations if donation.status == 'COMPLETE']
         }
 
     def __repr__ (self):
@@ -435,11 +435,11 @@ class Transactions(db.Model):
     def update_status(self):
         approvals = self.approvals
         if any(approval.approval_status is False for approval in approvals):
-            self.signatory_status = 'Pending'
+            self.signatory_status = 'Rejected'
         elif all(approval.approval_status for approval in approvals) and len(approvals) == 3:
             self.signatory_status = 'Approved'
         else:
-            self.signatory_status = 'Pending'
+            self.signatory_status = 'Awaiting'
         db.session.commit()
         return self.signatory_status
 
@@ -450,7 +450,7 @@ class TransactionApproval(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     transaction_id = db.Column(db.Integer, db.ForeignKey('transactions.id'), nullable=False)
     signatory_id = db.Column(db.Integer, db.ForeignKey('signatories.id'), nullable=False)
-    approval_status = db.Column(db.Boolean, default=False)
+    approval_status = db.Column(db.Boolean)
     approval_time = db.Column(db.DateTime, onupdate=db.func.now(), nullable=True)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now(), nullable=True)
@@ -462,26 +462,6 @@ class TransactionApproval(db.Model):
             'signatory_id': self.signatory_id,
             'approval_status': self.approval_status,
             'approval_time': self.approval_time,
-            'transaction': {
-                'id': self.transaction.id,
-                'tracking_id': self.transaction.tracking_id,
-                'batch_status': self.transaction.batch_status,
-                'trans_type': self.transaction.trans_type,
-                'trans_status': self.transaction.trans_status,
-                'amount': self.transaction.amount,
-                'acc_refence': self.transaction.acc_refence,
-                'narrative': self.transaction.narrative,
-                'transaction_account_no': self.transaction.transaction_account_no,
-                'request_ref_id': self.transaction.request_ref_id,
-                'name': self.transaction.name,
-                'transaction_date': self.transaction.transaction_date,
-                'org_id': self.transaction.org_id,
-                'campaign_name': self.transaction.campaign_name,
-                'bank_code': self.transaction.bank_code,
-                'signatory_status': self.transaction.signatory_status,
-                'created_at': self.transaction.created_at,
-                'updated_at': self.transaction.updated_at
-            },
             'created_at': self.created_at,
             'updated_at': self.updated_at
         }
